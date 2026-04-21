a2700e862ebb26cfaf14e4c18f0826d1a35a7b17
7994045 <34032171+7994045@users.noreply.github.com>
2026-04-21 18:05:13 +0300

    Fix DefaultBotSession import

--- src/main/java/com/toursearch/config/TelegramBotConfig.java
+++ src/main/java/com/toursearch/config/TelegramBotConfig.java
@@ -4,9 +4,8 @@ import com.toursearch.bot.TourBot;
 import org.springframework.context.annotation.Configuration;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.bots.DefaultBotSession;
+import org.telegram.telegrambots.DefaultBotSession;
 import jakarta.annotation.PostConstruct;
-
 
 @Configuration
 public class TelegramBotConfig {
