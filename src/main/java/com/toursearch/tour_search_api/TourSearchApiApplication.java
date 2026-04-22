5f93e98f68b391bf35ea49478829e71a524fe552
7994045 <34032171+7994045@users.noreply.github.com>
2026-04-22 17:49:53 +0300

    fix: add ComponentScan to discover all packages

--- src/main/java/com/toursearch/tour_search_api/TourSearchApiApplication.java
+++ src/main/java/com/toursearch/tour_search_api/TourSearchApiApplication.java
@@ -2,8 +2,10 @@ package com.toursearch.tour_search_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.toursearch")
 public class TourSearchApiApplication {
 
     public static void main(String[] args) {
