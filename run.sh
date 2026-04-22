a47942495fd15c145a6bd194d2cbbda4755af51c
7994045 <34032171+7994045@users.noreply.github.com>
2026-04-22 18:35:29 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@
 kill -9 $(lsof -t -i:8080) 2>/dev/null
 
 # === ВСТАВЬ СВОИ ТОКЕНЫ НИЖЕ ===
-export TELEGRAM_BOT_TOKEN=ВСТАВЬ_ТОКЕН_ОСНОВНОГО_БОТА
-export ADMIN_BOT_TOKEN=ВСТАВЬ_ТОКЕН_АДМИН_БОТА
+export TELEGRAM_BOT_TOKEN=8545778654:AAGY0DcVeHRlkchb10cNy1jqJAT5tvO9Ibg
+export ADMIN_BOT_TOKEN=8544818775:AAFFUK1nZbJdklhD1ddCwHX_8bD1ZhgCSf8
 # ================================
 
 nohup java -jar target/tour-search-api-0.0.1-SNAPSHOT.jar > app.log 2>&1 &
