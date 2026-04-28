022fc2bc35f835be349810ad8fa445d8d8c32208
7994045 <34032171+7994045@users.noreply.github.com>
2026-04-28 23:12:20 +0300

    feat: add UserRepository

---
+++ src/main/java/com/toursearch/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.toursearch.repository;
+
+import com.toursearch.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByTelegramId(Long telegramId);
+    boolean existsByTelegramId(Long telegramId);
+}
